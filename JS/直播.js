0d4f6c3a38f6ad85f820aec2eb728e8cbcafe756
lem85930 <21144917@qq.com>
2023-03-16 14:16:20 +0000

    更新 'JS/直播.js'

--- JS/直播.js
+++ JS/直播.js
@@ -1,6 +1,6 @@
 [
 {"name": "雷蒙",     "url": "https://agit.ai/n3rd/N3RD/raw/branch/master/JN/tv.txt"},
-{"name": "饭太硬",   "url": "http://ftyyy.tk/live.txt"}
+{"name": "饭太硬",   "url": "http://ftyyy.tk/live.txt"},
 {"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt"},
 {"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt"},
 {"name": "巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt"},
@@ -8,5 +8,5 @@
 {"name": "多多",     "url": "https://yydsys.top/duo/txt/v.txt"},
 {"name": "菜妮丝",   "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt"},
 {"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt"},
-{"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"},
+{"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"}
 ]
