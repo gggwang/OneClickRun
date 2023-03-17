b0566260c9bcae2c3fea08a1218b9ebf385aeb7b
lem85930 <21144917@qq.com>
2023-03-17 03:44:00 +0000

    更新 'JS/直播.js'

--- JS/直播.js
+++ JS/直播.js
@@ -1,7 +1,12 @@
 [
 {"name":"雷蒙",     "url": "https://agit.ai/n3rd/N3RD/raw/branch/master/JN/tv.txt"},
+{"name":"全球",   "url": "https://ghproxy.com/raw.githubusercontent.com/fanmingming/live/main/tv/m3u/global.m3u"},
+{"name":"4K8K",   "url": "https://ghproxy.com/https://raw.githubusercontent.com/youshandefeiyang/IPTV/main/main/IPTV.m3u"},
+{"name":"重庆有线",   "url": "https://ghproxy.com/https://raw.githubusercontent.com/youshandefeiyang/IPTV/main/main/cqyx.m3u"},
+{"name":"百事通",   "url": "https://telegram-feiyangdigital.v1.mk/bestv.m3u"},
+{"name":"APTV",   "url": "https://ghproxy.com/https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/aptv-playback.m3u"},
+{"name":"YanG",   "url": "https://ghproxy.com/https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u"},
 {"name":"饭太硬",   "url": "http://ftyyy.tk/live.txt"},
-{"name":"范明明",   "url": "https://ghproxy.com/raw.githubusercontent.com/fanmingming/live/main/tv/m3u/global.m3u"},
 {"name":"蜂蜜",     "url": "http://home.jundie.top:81/Cat/tv/live.txt"},
 {"name":"肥猫",     "url": "http://我不是.肥猫.love:63/TV/tvzb.txt"},
 {"name":"巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt"},
