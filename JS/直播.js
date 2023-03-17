fdf267060e414156fb60360a779d4a2506ef6165
lem85930 <21144917@qq.com>
2023-03-17 03:31:09 +0000

    更新 'JS/直播.js'

--- JS/直播.js
+++ JS/直播.js
@@ -1,12 +1,13 @@
 [
-{"name": "雷蒙",     "url": "https://agit.ai/n3rd/N3RD/raw/branch/master/JN/tv.txt"},
-{"name": "饭太硬",   "url": "http://ftyyy.tk/live.txt"},
-{"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt"},
-{"name": "肥猫",     "url": "http://我不是.肥猫.love:63/TV/tvzb.txt"},
-{"name": "巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt"},
-{"name": "星辰",     "url": "http://tvmvip.com/xclive.txt"},
-{"name": "多多",     "url": "https://yydsys.top/duo/txt/v.txt"},
-{"name": "菜妮丝",   "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt"},
-{"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt"},
-{"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"}
+{"name":"雷蒙",     "url": "https://agit.ai/n3rd/N3RD/raw/branch/master/JN/tv.txt"},
+{"name":"饭太硬",   "url": "http://ftyyy.tk/live.txt"},
+{"name":"范明明",   "url": "https://ghproxy.com/raw.githubusercontent.com/fanmingming/live/main/tv/m3u/global.m3u"},
+{"name":"蜂蜜",     "url": "http://home.jundie.top:81/Cat/tv/live.txt"},
+{"name":"肥猫",     "url": "http://我不是.肥猫.love:63/TV/tvzb.txt"},
+{"name":"巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt"},
+{"name":"星辰",     "url": "http://tvmvip.com/xclive.txt"},
+{"name":"多多",     "url": "https://yydsys.top/duo/txt/v.txt"},
+{"name":"菜妮丝",   "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt"},
+{"name":"乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt"},
+{"name":"吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"}
 ]
